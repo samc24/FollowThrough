b840cf53b858538e9c60fa98905816883f8ce573
samc24 <sam.chaturvedi24@gmail.com>
2018-04-23 18:30:11 -0400

    Record and Upload User Video

--- app/src/main/java/com/example/samch/followthrough/HomePage.java
+++ app/src/main/java/com/example/samch/followthrough/HomePage.java
@@ -16,26 +16,42 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toolbar;
 
-public class HomePage extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
+import io.objectbox.Box;
+
+public class HomePage extends AppCompatActivity {
+    private Box<Player> playersBox;
+    private List<Player> players;
+    private ArrayList<String> playerNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         setTheme(R.style.AppTheme_AB);
+//        playersBox = (MyObjectBox.builder().androidContext(HomePage.this).build()).boxFor(Player.class);
+//        players = playersBox.query().build().find();
+//        for (int i =0; i<players.size();i++){
+//            playerNames.add(players.get(i).getPlayerName());
+//        }
         // sort pros array (mergesort?)
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF27AE60")));
         ArrayAdapter adapter = (new ArrayAdapter<String>(this, R.layout.list_item,
                 getResources().getStringArray(R.array.pros)));
+//        ArrayAdapter adapter = (new ArrayAdapter<String>(this, R.layout.list_item,
+//                (String[])  playerNames.toArray()));
         ListView lv = findViewById(R.id.proList);
         lv.setAdapter(adapter);
+
         //lv.setTextFilterEnabled(true);
         lv.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(getApplicationContext(),((TextView)view).getText(), Toast.LENGTH_SHORT).show();
                 Intent compare = new Intent(HomePage.this, UserVPro.class);
-                compare.putExtra("Pro", (((TextView) view).getText()));
+                compare.putExtra("Pro", ((TextView)view).getText());
                 startActivity(compare);
             }
         } );
--- app/src/main/java/com/example/samch/followthrough/UserVPro.java
+++ app/src/main/java/com/example/samch/followthrough/UserVPro.java
@@ -15,8 +15,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import io.objectbox.Box;
+
 public class UserVPro extends AppCompatActivity {
     public static final int PICK_VIDEO_REQUEST = 1;
+    public Button choose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,9 @@ public class UserVPro extends AppCompatActivity {
         setContentView(R.layout.activity_user_vpro);
         final ImageButton pro = findViewById(R.id.pro);
         final VideoView mVideoView1 =  findViewById(R.id.videoView1);
+//        long playerId = getIntent().getExtras().getLong("Pro");
+//        Box<Player> playersBox = (MyObjectBox.builder().androidContext(UserVPro.this).build()).boxFor(Player.class);
+//        final Player player = playersBox.get(playerId);
         String uriPath1 = "android.resource://com.example.samch.followthrough/";
         String form = getIntent().getExtras().getString("Pro");
         switch(form){
@@ -36,11 +42,11 @@ public class UserVPro extends AppCompatActivity {
              default:
                  uriPath1 += R.raw.dameform;
         }
+//        final Uri uri1 = Uri.parse(player.getLocalFileURI());
         final Uri uri1 = Uri.parse(uriPath1);
         pro.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 pro.setVisibility(View.INVISIBLE);
                 mVideoView1.setVideoURI(uri1);
                 mVideoView1.requestFocus();
@@ -48,12 +54,33 @@ public class UserVPro extends AppCompatActivity {
 
                     @Override
                     public void onPrepared(MediaPlayer mp) {
+                        /*
+                         * NEW
+                         */
+                        int videoWidth = mp.getVideoWidth();
+                        int videoHeight = mp.getVideoHeight();
+                        float videoProportion = (float) videoWidth / (float) videoHeight;
+                        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+                        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+                        float screenProportion = (float) screenWidth / (float) screenHeight;
+                        android.view.ViewGroup.LayoutParams lp = mVideoView1.getLayoutParams();
+                        if (videoProportion > screenProportion) {
+                            lp.width = screenWidth;
+                            lp.height = (int) ((float) screenWidth / videoProportion);
+                        } else {
+                            lp.width = (int) (videoProportion * (float) screenHeight);
+                            lp.height = screenHeight;
+                        }
+                        mVideoView1.setLayoutParams(lp);
+
+
+                        /*
+                         *
+                         */
 
                         mp.setVolume(0, 0);
                     }
                 });
-
-
                 mVideoView1.start();
             }
         });
@@ -72,7 +99,7 @@ public class UserVPro extends AppCompatActivity {
                // mVideoView1.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT));
             }
         });
-        final Button choose = findViewById(R.id.choose);
+        choose = findViewById(R.id.choose);
         choose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,7 +144,20 @@ public class UserVPro extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PICK_VIDEO_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
+                final VideoView mVideoView2 =  findViewById(R.id.videoView2);
+                mVideoView2.setVisibility(View.VISIBLE);
+                final ImageButton user = findViewById(R.id.user);
+                user.setVisibility(View.VISIBLE);
+                final ImageButton userfs = findViewById(R.id.userfs);
+                userfs.setVisibility(View.VISIBLE);
+                choose.setVisibility(View.GONE);
+                String path = data.getData().toString();
+                Toast.makeText(getApplicationContext(),path, Toast.LENGTH_SHORT).show();
+                mVideoView2.setVideoPath(path);
+                user();
+                //mVideo.requestFocus();
+                //mVideo.start();
                 // The Intent's data Uri identifies which contact was selected.
 
                 // Do something here
@@ -129,20 +169,20 @@ public class UserVPro extends AppCompatActivity {
 
         final ImageButton user = findViewById(R.id.user);
         final VideoView mVideoView2 =  findViewById(R.id.videoView2);
-        String uriPath2 = "android.resource://com.example.samch.followthrough/" + R.raw.dameform;
-        final Uri uri2 = Uri.parse(uriPath2);
 
         user.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 user.setVisibility(View.INVISIBLE);
-                mVideoView2.setVideoURI(uri2);
                 mVideoView2.requestFocus();
                 mVideoView2.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 
                     @Override
                     public void onPrepared(MediaPlayer mp) {
+
+
+
 
                         mp.setVolume(0, 0);
                     }
